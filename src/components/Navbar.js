c67e36733220aa296457db2f84ce317844a14882
JakariaJishan <jakariajishan987@gmail.com>
2023-03-10 01:02:49 +0600

    Add style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/Navbar.module.css";
 function Navbar() {
   return (
-    <div>
-      <Link to="/">Books</Link>
-      <Link to="/categories">Categories</Link>
+    <div className={styles.panelBg}>
+      <div className={styles.mainPanel}>
+        <Link to="/" className={styles.branding}>
+          Bookstore CMS
+        </Link>
+        <Link to="/" className={styles.links}>Books</Link>
+        <Link to="/categories" className={styles.links}>Categories</Link>
+      </div>
+      <Link to='#'>icon</Link>
     </div>
   );
 }

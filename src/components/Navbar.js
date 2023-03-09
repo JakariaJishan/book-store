3da6cc4b5403e40f9bb95af13d78692c2e1692f1
JakariaJishan <jakariajishan987@gmail.com>
2023-03-10 03:28:41 +0600

    Customize navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../styles/Navbar.module.css";
+
 function Navbar() {
   return (
     <div className={styles.panelBg}>
-      <div className={styles.mainPanel}>
-        <Link to="/" className={styles.branding}>
-          Bookstore CMS
+      <div className={styles.innerPanel}>
+        <div className={styles.mainPanel}>
+          <Link to="/" className={styles.branding}>
+            Bookstore CMS
+          </Link>
+          <Link to="/" className={styles.links}>
+            Books
+          </Link>
+          <Link to="/categories" className={styles.links}>
+            Categories
+          </Link>
+        </div>
+        <Link to="#">
+          <img src="/user.png" />
         </Link>
-        <Link to="/" className={styles.links}>Books</Link>
-        <Link to="/categories" className={styles.links}>Categories</Link>
       </div>
-      <Link to='#'>icon</Link>
     </div>
   );
 }

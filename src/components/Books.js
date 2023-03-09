5b237c3d8d038e72983d0f5bc1f33566850485ea
JakariaJishan <jakariajishan987@gmail.com>
2023-03-10 01:13:51 +0600

    Add style to booklists

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Book from './Book';
-import Form from './Form';
+import React from "react";
+import { useSelector } from "react-redux";
+import styles from '../styles/Books.module.css';
+import Book from "./Book";
+import Form from "./Form";
 
 function Books() {
   const books = useSelector((state) => state.books);
   return (
-    <div>
-      {books.isLoading && <h3>Loading books ...</h3>}
-      {!books.isLoading && books.errorMsg ? (
-        <h3>
-          Error:
-          {books.errorMsg}
-        </h3>
-      ) : null}
-      {!books.isLoading && books.bookList
-        ? Object.entries(books.bookList).map((book, index) => (
-          <Book key={Math.random()} book={book} />
-        ))
-        : null}
+    <div className={styles.panel}>
+      <div>
+        {books.isLoading && <h3>Loading books ...</h3>}
+        {!books.isLoading && books.errorMsg ? (
+          <h3>
+            Error:
+            {books.errorMsg}
+          </h3>
+        ) : null}
+        {!books.isLoading && books.bookList
+          ? Object.entries(books.bookList).map((book, index) => (
+              <Book key={Math.random()} book={book} />
+            ))
+          : null}
+      </div>
+
       <Form />
     </div>
   );
